429f27e0ef92c620bc4bc3460cbb88b9b612c3f4
Martin Zabinski <m.zabinski@gmx.net>
2015-09-05 23:28:20 +0200

    neuern Livemodus

--- main.php
+++ main.php
@@ -4,15 +4,15 @@
         <title>Online Codex2</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="js/libs/jquery-mobile/jquery.mobile.css">
+        <link rel="stylesheet" href="js/libs/jquery-mobile/jquery.mobile<?php echo $live ? '.min' : '' ?>.css">
         <link rel="stylesheet" href="css/onlinecode2.css">
-        <script src="js/libs/jquery/jquery.js"></script>
-        <script src="js/libs/jquery-mobile/jquery.mobile.js"></script>
+        <script src="js/libs/jquery/jquery<?php echo $live ? '.min' : '' ?>.js"></script>
+        <script src="js/libs/jquery-mobile/jquery.mobile<?php echo $live ? '.min' : '' ?>.js"></script>
         <script src="js/onlinecodex2.js"></script>
         <script type="text/javascript">
           $(document).ready(function() {
             // kleiner Test
-            var indexJson = getArmyIndex();
+            var indexJson = getArmyIndex(<?php echo $live ? 'true' : 'false' ?>);
             if(console && console.log) {
               console.log(typeof indexJson);
               console.log(indexJson);
