af7b65839cbf5ccb4e676e94a212fdfa3bc15c4b
Martin Zabinski <m.zabinski@gmx.net>
2015-09-09 17:13:59 +0200

    2015-09-09 master

---
+++ js/onlinecodex2.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function get(uri, callback) {
+  uri = typeof uri !== "undefined" ? uri : "";
+  callback = typeof callback !== "undefined" ? callback : false;
+  var ajax = $.ajax({
+    method: "GET",
+    url: uri,
+    dataType: "json",
+    async: (callback === false ? false : true)
+  });
+  if(callback === false) {
+    var returnValue;
+    ajax.done(function (data){
+      returnValue = data;
+    });
+    return returnValue;
+  } else {
+    ajax.done(callback);
+    return callback;
+  }
+}
+
+function getArmyIndex(live) {
+  return get("codex/index.json" + (live === true ? ".php" : ""));
+}
